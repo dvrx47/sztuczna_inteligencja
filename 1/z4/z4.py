ddba9c8b8389bc30f6667e3bc48a829c8851c110
dvrx47 <kowalik_mikolaj@wp.pl>
2018-03-18 23:18:17 +0100

    optymalizacja

--- 1/z4/z4.py
+++ 1/z4/z4.py
@@ -1,21 +1,16 @@
 def ones( binary_word ):
-	count = 0
-	for x in binary_word:
-		if x=='1':
-			count += 1
-	return count
+	res = [0]
+	for i in binary_word:
+		res.append( res[-1] + i )
+	return res
+	
 
 def opt_dist( binary_word, D):
 	min_val = len(binary_word)
+	ca = ones( binary_word )
 	
-	for i in range(0, len(binary_word) - D+1):
-		cost = 0
-		cost += ones( binary_word[:i] )
-		cost += D - ones(binary_word[i:i+D])
-		cost += ones(binary_word[i+D:])
+	for i in range(1, len(binary_word) - D + 1):
+		cost = D + ca[-1] - 2*ca[i+D-1]+2*ca[i-1]
 		if cost < min_val:
 			min_val = cost
 	return min_val
-
-for i in [5,4,3,2,1,0]:
-	print(opt_dist('0010001000', i))
